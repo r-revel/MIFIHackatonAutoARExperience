aa4e4e140dbf358473855e015f2a457459f7e90f
Happy <crazyhappy007@gmail.com>
2024-12-28 19:05:42 +1000

    add dev config

--- Service/DetectionService/main.py
+++ Service/DetectionService/main.py
@@ -100,10 +100,3 @@ async def detect(request: DetectRequest) -> DetectResponce:
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
-
-if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8000)
